51903c11a783fcf238254a3eeb20f4d792086694
paisoncodes <iamkomolafe.o.s@gmail.com>
2025-10-31 13:45:33 +0100

    changes

--- myScript.js
+++ myScript.js
@@ -2,5 +2,16 @@ function scrollToBoth() {
   document.getElementById("logo-and-navbar").scrollIntoView({ behavior: "smooth" });
   setTimeout(() => {
     document.getElementById("Hero").scrollIntoView({ behavior: "smooth" });
-  });
+  }, 1000);
 }
+
+ document.getElementById("scrollToEnd").addEventListener("click", function(event) {
+      event.preventDefault();
+      const section = document.getElementById("about");
+      const sectionBottom = section.offsetTop + section.offsetHeight;
+
+      window.scrollTo({
+        top: sectionBottom,
+        behavior: "smooth" // makes the scroll smooth
+      });
+    });
